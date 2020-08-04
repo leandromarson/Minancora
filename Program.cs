0aababefa30da947c0dd8f4a454cb995725d96c9
Leandro Marson Ribeiro <leandromarson1@hotmail.com>
2020-08-04 19:30:28 -0300

    googol

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Minancora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(long x = 0; x <= Math.Pow(10, 100); x++){
+                Console.Write(x+"\n");
+            }
+        }
+    }
+}
